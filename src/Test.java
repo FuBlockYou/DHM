c442c2d0aa53c306b9024f9c3acc939a5311b93a
FuBlockYou <980709500@qq.com>
2018-07-03 11:20:46 +0800

    add Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,4 @@
-public class Test {
+﻿public class Test {
 
 
     public static void main(String[] args) {
@@ -6,5 +6,6 @@ public class Test {
         System.out.println("qiuzhechousile");	
 	System.out.println("添加了语音识别");
 	System.out.println("添加了人脸识别");
+	System.out.println("A分支");
     }
 }
