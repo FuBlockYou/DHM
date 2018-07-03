36a26dc8c0cdac9dd196aae134788f6a0a4c8443
FuBlockYou <980709500@qq.com>
2018-07-03 11:35:57 +0800

    A

---
+++ src/Test2.java
@@ -0,0 +1,10 @@
+public class Test2{
+	public static void main(String[] args){
+		  System.out.println("sssss");
+		  System.out.println("qiuzhechousile");	
+	      System.out.println("添加了语音识别");
+	      System.out.println("添加了人脸识别");
+	      System.out.println("A分支");
+		  System.out.println("A分支继续搞事情");
+	}
+}
