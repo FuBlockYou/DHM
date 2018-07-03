60d6c9e5996f0a09f7728ab95374f90bb89379c4
FuBlockYou <980709500@qq.com>
2018-07-03 10:38:57 +0800

    initial commit

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("sssss");
-        System.out.println("qiuzhechousile");
+        System.out.println("qiuzhechousile");	
+	System.out.println("语音识别功能");
     }
 }
